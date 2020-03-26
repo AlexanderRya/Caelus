64056e651460e7f5dda6e437103846685a7135f0
Alexander <alexandr.benbaccar@gmail.com>
2020-03-26 22:50:45 +0100

    uhm, ok.

--- src/engine/core/api/VulkanMemoryAllocator.cpp
+++ src/engine/core/api/VulkanMemoryAllocator.cpp
@@ -1,8 +1,8 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Weverything"
 
-#define VMA_STATIC_VULKAN_FUNCTIONS 0
 #define VMA_IMPLEMENTATION
+#define VMA_STATIC_VULKAN_FUNCTIONS 0
 #include <vk_mem_alloc.h>
 
 #pragma clang diagnostic pop
